12079660482f026450a59a5da5097ffbc5741890
euclides <euclideskinto@gmail.com>
2023-01-12 14:20:01 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="items-center flex bg-sky-500 py-5 w-full">
+    <div class="container mx-auto">
+      <h1 class="text-2xl text-white">Vue Projects</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
